ac2e5cc7606eb49c0b1e95ca7f242f425c4ecee0
Vladimir Curiel <vladimircuriel@outlook.com>
2025-04-08 20:05:22 +0000

    fix(main): update environment variable paths for consistency and clarity

--- src/logicmate/main.py
+++ src/logicmate/main.py
@@ -5,11 +5,11 @@ from logicmate.utils.logger.logger import setup_logging
 
 def main(video_path: str) -> None:
     setup_logging()
-    load_env_variables(env_path=".env")
+    load_env_variables(env_path="/teamspace/studios/this_studio/LogicMate-v3/.env")
     config: dict[str, str] = get_required_env_variables()
     start_bot(video_path=video_path, config=config)
 
 
 if __name__ == "__main__":
-    video_path = "media/videos/21a10a54-4ac2-4eb8-8807-dcfbaab749ca.mp4"
+    video_path = "/teamspace/studios/this_studio/LogicMate-v3/media/videos/21a10a54-4ac2-4eb8-8807-dcfbaab749ca.mp4"
     main(video_path=video_path)
